4c38541538fb47338c9122fbb304c6530f12ba99
fixedlight <fixedlight@gmail.com>
2019-07-13 14:27:33 +0100

    fixed naming of arguments

--- lib/src/main/kotlin/com/fsbarata/rx2/tools/RxCompletes.kt
+++ lib/src/main/kotlin/com/fsbarata/rx2/tools/RxCompletes.kt
@@ -6,9 +6,9 @@ import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicInteger
 
 fun <T> Observable<T>.repeatWith(transformation: (Any) -> Single<Any>) = repeatWhen { it.flatMapSingle(transformation) }
-fun <T> Single<T>.repeatWith(transformation: (Any) -> Single<Any>) = repeatWhen { errors -> errors.flatMapSingle(transformation) }
-fun <T> Maybe<T>.repeatWith(transformation: (Any) -> Single<Any>) = repeatWhen { errors -> errors.flatMapSingle(transformation) }
-fun Completable.repeatWith(transformation: (Any) -> Single<Any>) = repeatWhen { errors -> errors.flatMapSingle(transformation) }
+fun <T> Single<T>.repeatWith(transformation: (Any) -> Single<Any>) = repeatWhen { it.flatMapSingle(transformation) }
+fun <T> Maybe<T>.repeatWith(transformation: (Any) -> Single<Any>) = repeatWhen { it.flatMapSingle(transformation) }
+fun Completable.repeatWith(transformation: (Any) -> Single<Any>) = repeatWhen { it.flatMapSingle(transformation) }
 
 fun <T> Observable<T>.repeatDelayed(delay: Long, timeUnit: TimeUnit, scheduler: Scheduler = Schedulers.computation()) =
 		repeatWith(delayTransformation(delay, timeUnit, scheduler))
